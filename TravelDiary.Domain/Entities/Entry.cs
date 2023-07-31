33779df292ddbd2e20bc5bc1f7ea2c36684ac8ab
WojciechLiszka <wliszka99@gmail.com>
2023-07-31 23:34:13 +0200

    added Entry entity model

---
+++ TravelDiary.Domain/Entities/Entry.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelDiary.Domain.Entities
+{
+    public class Entry
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public string Description { get; set; } = null!;
+        public DateTime Date { get; set; }
+    }
+}
