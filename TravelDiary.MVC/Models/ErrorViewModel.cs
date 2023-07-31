de0edc67986b4d60a2f73f07695c24318dc8fa62
WojciechLiszka <wliszka99@gmail.com>
2023-07-31 21:22:35 +0200

    added TravelDiary.MVC

---
+++ TravelDiary.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelDiary.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
