0641c181bff6df43ec13a36cc70dde9ae76d9d2e
WojciechLiszka <wliszka99@gmail.com>
2023-07-31 22:54:21 +0200

    added Diary domain model

--- TravelDiary.Domain/Entities/User.cs
+++ TravelDiary.Domain/Entities/User.cs
@@ -9,10 +9,10 @@ namespace TravelDiary.Domain.Entities
     public class User
     {
         public Guid Id { get; set; }
-        public string NickName { get; set; }
-        public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set;}
-        public string PasswordHash { get; set; }
+        public string NickName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set;} = null!;
+        public string PasswordHash { get; set; } = null!;
     }
 }
