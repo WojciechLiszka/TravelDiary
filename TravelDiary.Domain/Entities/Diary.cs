087562b985a82267e1feb762672851886986b29c
WojciechLiszka <wliszka99@gmail.com>
2023-08-01 13:26:29 +0200

    updated domain entitites modles

--- TravelDiary.Domain/Entities/Diary.cs
+++ TravelDiary.Domain/Entities/Diary.cs
@@ -2,13 +2,12 @@
 {
     public class Diary
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; } = null!;
         public bool IsPublic { get; set; }
         public DateTime Starts { get; set; }
         public DateTime Ends { get; set; }
         public string Description { get; set; } = null!;
-
-
+        public PrivacyPolicy Policy { get; set; } = PrivacyPolicy.Private;
     }
 }
--- TravelDiary.Domain/Entities/Entry.cs
+++ TravelDiary.Domain/Entities/Entry.cs
@@ -2,8 +2,8 @@
 {
     public class Entry
     {
-        public int Id { get; set; }
-        public string Tittle { get; set; }
+        public Guid Id { get; set; }
+        public string Tittle { get; set; } = null!;
         public string Description { get; set; } = null!;
         public DateTime Date { get; set; }
     }
