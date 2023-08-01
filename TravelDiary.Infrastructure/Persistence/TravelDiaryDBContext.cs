c7866ad54688a8cca775706ed6e71ce5c970c593
WojciechLiszka <wliszka99@gmail.com>
2023-08-01 13:07:53 +0200

    Created DB_Context

---
+++ TravelDiary.Infrastructure/Persistence/TravelDiaryDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelDiary.Domain.Entities;
+
+namespace TravelDiary.Infrastructure.Persistence
+{
+    public class TravelDiaryDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Diary> Diaries { get; set; }
+    }
+}
