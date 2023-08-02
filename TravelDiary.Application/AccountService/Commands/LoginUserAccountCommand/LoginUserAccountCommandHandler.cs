2f1604503e784f43c5aecb6ca92113bf0705d0af
WojciechLiszka <wliszka99@gmail.com>
2023-08-02 23:05:43 +0200

    added authorization

--- TravelDiary.Application/AccountService/Commands/LoginUserAccountCommand/LoginUserAccountCommandHandler.cs
+++ TravelDiary.Application/AccountService/Commands/LoginUserAccountCommand/LoginUserAccountCommandHandler.cs
@@ -1,15 +1,24 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Identity;
 using TravelDiary.Domain.Entities;
+using TravelDiary.Domain.Interfaces;
+using TravelDiary.Domain.Models;
 
 namespace TravelDiary.Application.AccountService.Commands.LoginUserAccountCommand
 {
     public class LoginUserAccountCommandHandler : IRequestHandler<LoginUserAccountCommand, string>
     {
+        private readonly IAccountRepository _accountRepository;
+        private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly AuthenticationSettings _authenticationSettings;
+
         public LoginUserAccountCommandHandler(IAccountRepository repository, IPasswordHasher<User> passwordHasher, AuthenticationSettings authenticationSettings)
         {
-            
+            _accountRepository = repository;
+            _passwordHasher = passwordHasher;
+            _authenticationSettings = authenticationSettings;
         }
+
         public Task<string> Handle(LoginUserAccountCommand request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
--- TravelDiary.Application/Extensions/ServiceCollectionExtension.cs
+++ TravelDiary.Application/Extensions/ServiceCollectionExtension.cs
@@ -3,8 +3,10 @@ using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System.Reflection;
 using TravelDiary.Domain.Entities;
+using TravelDiary.Domain.Models;
 
 namespace TravelDiary.Application.Extensions
 {
@@ -13,10 +15,35 @@ namespace TravelDiary.Application.Extensions
         public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
-            services.AddFluentValidationAutoValidation();
 
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            var authenticationSettings = new AuthenticationSettings();
+
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
+            configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+            services.AddSingleton(authenticationSettings);
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = "Bearer";
+                option.DefaultScheme = "Bearer";
+                option.DefaultChallengeScheme = "Bearer";
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = authenticationSettings.JwtIssuer,
+                    ValidAudience = authenticationSettings.JwtIssuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+                };
+            });
+
+            services.AddAuthorization();
         }
     }
 }
