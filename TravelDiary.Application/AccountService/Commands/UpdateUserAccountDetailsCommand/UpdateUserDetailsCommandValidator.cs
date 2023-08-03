ad99de7b85757641ce28a14ba039c709dac3d2cb
WojciechLiszka <wliszka99@gmail.com>
2023-08-03 15:07:15 +0200

    aded UpdateUserDetailsCommandValidator

---
+++ TravelDiary.Application/AccountService/Commands/UpdateUserAccountDetailsCommand/UpdateUserDetailsCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace TravelDiary.Application.AccountService.Commands.UpdateUserAccountDetailsCommand
+{
+    public class UpdateUserDetailsCommandValidator : AbstractValidator<UpdateUserDetailsCommand>
+    {
+        public UpdateUserDetailsCommandValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotNull()
+                .NotEmpty()
+                .Length(3, 20);
+
+            RuleFor(x => x.LastName)
+                .NotNull()
+                .NotEmpty()
+                .Length(3, 20);
+
+            RuleFor(x => x.Country)
+                .NotNull()
+                .NotEmpty()
+                .Length(3, 20);
+        }
+    }
+}
