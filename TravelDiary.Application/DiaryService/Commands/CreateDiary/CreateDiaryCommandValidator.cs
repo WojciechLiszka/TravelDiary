393ef9771a2eff2cb102eddd7e0cd1a72c13619d
WojciechLiszka <wliszka99@gmail.com>
2023-08-11 09:55:30 +0200

    added CreateDiaryCommandValidator

---
+++ TravelDiary.Application/DiaryService/Commands/CreateDiary/CreateDiaryCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace TravelDiary.Application.DiaryService.Commands.CreateDiary
+{
+    public class CreateDiaryCommandValidator : AbstractValidator<CreateDiaryCommand>
+    {
+        public CreateDiaryCommandValidator()
+        {
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(1)
+                .MaximumLength(500);
+
+            RuleFor(x => x.Name)
+                .NotNull()
+                .NotEmpty()
+                .MinimumLength(5)
+                .MaximumLength(50);
+
+            RuleFor(x => x.Starts)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Policy)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
