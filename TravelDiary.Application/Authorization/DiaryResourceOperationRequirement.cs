d3da0d99fd173c6b22cc18469ee5a63f227ad257
WojciechLiszka <wliszka99@gmail.com>
2023-08-11 08:58:04 +0200

    added DiaryResourceRequirement - authorization requirement

---
+++ TravelDiary.Application/Authorization/DiaryResourceOperationRequirement.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using TravelDiary.Domain.Models;
+
+namespace TravelDiary.Application.Authorization
+{
+    public class  DiaryResourceOperationRequirement:IAuthorizationRequirement
+
+    {
+        public ResourceOperation Operation { get; set; }
+        public DiaryResourceOperationRequirement(ResourceOperation resourceOperation)
+        {
+            Operation = resourceOperation;
+        }
+    }
+
+
+
+}
