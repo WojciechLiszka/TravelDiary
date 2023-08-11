d6518d92e262963efe18f8f0f4a3ce4a6965ea66
WojciechLiszka <wliszka99@gmail.com>
2023-08-11 12:02:24 +0200

    added UpdateDescription_ForValidIdAndDescription_ReturnsOK test

--- TravelDiary.ApiTests/Controllers/DiaryControllerTest.cs
+++ TravelDiary.ApiTests/Controllers/DiaryControllerTest.cs
@@ -255,6 +255,7 @@ namespace TravelDiary.ApiTests.Controllers
 
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.NoContent);
         }
+
         [Fact]
         public async Task Delete_ForInvalidId_ReturnsNotFound()
         {
@@ -297,6 +298,7 @@ namespace TravelDiary.ApiTests.Controllers
 
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
         }
+
         [Fact]
         public async Task Delete_ForNotDiaryOwner_ReturnsForbidden()
         {
@@ -353,6 +355,55 @@ namespace TravelDiary.ApiTests.Controllers
             //assert
 
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task UpdateDescription_ForValidIdAndDescription_ReturnsOK()
+        {
+            // arrange
+            var role = new UserRole()
+            {
+                RoleName = "User"
+            };
+            await SeedRole(role);
+
+            var user = new User()
+            {
+                NickName = "JDoe",
+                UserDetails = new UserDetails()
+                {
+                    Email = "test@email.com",
+                    Country = "USA",
+                    FirstName = "John",
+                    LastName = "Doe"
+                },
+
+                PasswordHash = "validPassword",
+                UserRoleId = role.Id,
+            };
+            await SeedUser(user);
+
+            await PrepareUserClient(user, role);
+
+            var diary = new Diary()
+            {
+                CreatedById = user.Id,
+                Description = "Description",
+                Name = "Name",
+                Starts = new DateTime(2008, 5, 1, 8, 30, 0),
+                Ends = new DateTime(2009, 5, 1, 8, 30, 0)
+            };
+            await SeedDiary(diary);
+
+            var description = "NewDescription";
+
+            var httpContent = description.ToJsonHttpContent();
+            //act
+
+            var response = await _userClient.PutAsync($"{_route}/{diary.Id}/Description",httpContent);
+            //assert
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
     }
 }
