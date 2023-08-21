b408e0b97d4acaec73b0af950e2873c09dd4a186
WojciechLiszka <wliszka99@gmail.com>
2023-08-21 10:07:54 +0200

    added Create_ForNotDiaryOwner_ReturnsForbidden test

--- TravelDiary.ApiTests/Controllers/EntryControllerTest.cs
+++ TravelDiary.ApiTests/Controllers/EntryControllerTest.cs
@@ -233,5 +233,78 @@ namespace TravelDiary.ApiTests.Controllers
 
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
         }
+
+        [Fact]
+        public async Task Create_ForNotDiaryOwner_ReturnsForbidden()
+        {
+            // arrange
+
+            var role = new UserRole()
+            {
+                RoleName = "User"
+            };
+            await SeedRole(role);
+
+            var user = new User()
+            {
+                NickName = "JDoe",
+                UserDetails = new UserDetails()
+                {
+                    Email = "test@email.com",
+                    Country = "USA",
+                    FirstName = "John",
+                    LastName = "Doe"
+                },
+
+                PasswordHash = "validPassword",
+                UserRoleId = role.Id,
+            };
+            await SeedUser(user);
+
+            var invalidUser = new User()
+            {
+                NickName = "JDoe",
+                UserDetails = new UserDetails()
+                {
+                    Email = "test@email.com",
+                    Country = "USA",
+                    FirstName = "John",
+                    LastName = "Doe"
+                },
+
+                PasswordHash = "validPassword",
+                UserRoleId = role.Id,
+            };
+
+            await SeedUser(invalidUser);
+
+            await PrepareUserClient(invalidUser, role);
+
+            var diary = new Diary()
+            {
+                CreatedById = user.Id,
+                Description = "Description",
+                Name = "Name",
+                Starts = new DateTime(2008, 5, 1, 8, 30, 0),
+                Ends = new DateTime(2009, 5, 1, 8, 30, 0)
+            };
+            await SeedDiary(diary);
+
+            var dto = new CreateEntryDto()
+            {
+                Tittle = "TestTittle",
+                Description = "Description",
+                Date = new DateTime(2008, 5, 1, 8, 30, 0)
+            };
+            var httpContent = dto.ToJsonHttpContent();
+
+            // act
+
+            var response = await _userClient.PostAsync($"{_route}/Diary/{diary.Id}/Entry", httpContent);
+
+            //assert
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.Forbidden);
+        }
     }
 }
